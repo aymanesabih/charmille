6c153c6576530986d4584acc2471d57495fe28c2
Zinebchibli <zineb3214chibli@gmail.com>
2024-05-17 16:45:29 +0100

    auth

---
+++ app/espace/accueil/page.js
@@ -0,0 +1,24 @@
+import Logout from "@/components/component/logout";
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+
+
+export default async function Home() {
+  const cookieStore =cookies();
+   const supabase = createServerComponentClient({cookies:( )=> cookieStore});
+   const {
+    data: { user },
+  } = await supabase.auth.getUser();
+   console.log({user})
+
+   if (!user){
+    return(
+    redirect("/espace/login")
+    )
+    
+   }
+  return (
+    <><p>hello,{user.email}, u created ur account at : {user.created_at}</p><Logout /></>
+  );
+}
---
+++ app/espace/page.js
@@ -0,0 +1,9 @@
+import Espace from '@/components/component/espace'
+import React from 'react'
+
+export default function page() {
+  return (
+    <Espace/>
+)
+}
+
