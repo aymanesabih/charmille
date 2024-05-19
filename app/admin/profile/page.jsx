ce79113e16d9696586fff416e56b1e7cf6efa5f5
Ayoub <88906465+AyoubTechs@users.noreply.github.com>
2024-05-20 00:28:09 +0100

    beta

---
+++ app/admin/profile/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { SearchPage } from './SearchPage';
+
+const page = () => {
+    return (
+        <SearchPage />)
+}
+
+export default page
--- components/component/AdditionalDetailsForm.js
+++ components/component/AdditionalDetailsForm.js
@@ -1,7 +1,7 @@
 // ./components/componentAdditionalDetailsForm.js
-import { Label } from "@/components/ui/label";
-import { Textarea } from "@/components/ui/textarea";
-import { Button } from "@/components/ui/button";
+import { Label } from "../ui/label";
+import { Textarea } from "../ui/textarea";
+// import { Button } from "@/components/ui/button";
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 
 
